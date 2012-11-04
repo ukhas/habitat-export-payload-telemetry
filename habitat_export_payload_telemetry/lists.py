c4c75e2098b9baaeefb661e641f07e0441e17fec
Adam Greig <adam@adamgreig.com>
2012-11-04 15:49:11 +0000

    properly escape csv output

--- habitat_export_payload_telemetry/lists.py
+++ habitat_export_payload_telemetry/lists.py
@@ -9,7 +9,9 @@ Used with the views:
     payload_telemetry/payload_time
 """
 
+import csv
 import json
+import StringIO
 from couch_named_python import version
 
 @version(1)
@@ -25,11 +27,13 @@ def json_list(head, req, rows):
         comma = True
     yield "\n]\n"
 
-@version(2)
+@version(3)
 def csv_list(head, req, rows):
     yield req['query']['fields'] + "\n"
     fields = req['query']['fields'].split(',')
     for row in rows:
+        buf = StringIO.StringIO()
+        writer = csv.writer(buf)
         data = row['doc']['data']
-        csv_row = [str(data.get(key, '')).strip() for key in fields]
-        yield ','.join(csv_row) + "\n"
+        writer.writerow([str(data.get(key, '')).strip() for key in fields])
+        yield buf.getvalue()
